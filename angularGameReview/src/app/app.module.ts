732f5ee9a53e2addbb38761dca54208a9e32d0c3
Jc2772 <jc2772@outlook.com>
2024-12-07 12:45:21 +0000

    .

--- angularGameReview/src/app/app.module.ts
+++ angularGameReview/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeListComponent } from './home-list/home-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
