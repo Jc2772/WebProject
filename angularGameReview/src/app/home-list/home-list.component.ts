87b19fc7dfdcc60ade779d1896a95c426accfaf0
Jc2772 <jc2772@outlook.com>
2024-12-07 00:08:17 +0000

    .

---
+++ angularGameReview/src/app/home-list/home-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-list',
+  standalone: false,
+  
+  templateUrl: './home-list.component.html',
+  styleUrl: './home-list.component.css'
+})
+export class HomeListComponent {
+
+}
