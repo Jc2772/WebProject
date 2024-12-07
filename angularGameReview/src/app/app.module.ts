7d4724686d5254a0564554243dbeab48e26e71ed
Jc2772 <jc2772@outlook.com>
2024-12-07 17:03:49 +0000

    modified:   angularGameReview/src/app/app.module.ts

--- angularGameReview/src/app/app.module.ts
+++ angularGameReview/src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpHeaders, HttpClientModule } from '@angular/common/http';
 import { HomeListComponent } from './home-list/home-list.component';
-import { DistancePipe } from './distance.pipe';
-
 imports: [
   BrowserModule,
   HttpClientModule
 ]
 @NgModule({
   declarations: [
-    HomeListComponent,
-    DistancePipe
+    HomeListComponent
   ],
   imports: [
     BrowserModule
