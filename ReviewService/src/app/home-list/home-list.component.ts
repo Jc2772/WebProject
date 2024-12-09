316215a5e4fd3c0ff5c122416d5421625d7733c2
Jc2772 <jc2772@outlook.com>
2024-12-09 15:00:22 +0000

    new file:   ReviewService/.editorconfig

---
+++ ReviewService/src/app/home-list/home-list.component.ts
@@ -0,0 +1,28 @@
+import {ReviewService} from '../review-service.service'
+import { Component, OnInit } from '@angular/core';
+export class Review{
+  game!: string;
+  description!: string;
+  avgscore!: number
+}
+@Component({
+  selector: 'app-home-list',
+  standalone: false,
+  
+  templateUrl: './home-list.component.html',
+  styleUrl: './home-list.component.css'
+})
+export class HomeListComponent implements OnInit {
+  constructor(private reviewservice :ReviewService){}
+  reviews: Review[] = []
+  private getReviews() : void{
+    this.reviewservice
+    .getReviews()
+    .then(foundReviews =>{
+      this.reviews = foundReviews
+    });
+  }
+  ngOnInit(): void {
+    this.getReviews()
+  }
+}
--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'app_public')))
 app.use('/api', function(req, res, next) {
   res.header('Access-Control-Allow-Origin','http://localhost:4200');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-')
   next();
 });
 app.use(passport.initialize());
@@ -49,6 +49,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-var Account = require('./')
-passport.use(new LocalStrategy(Account.SignIn()))
 module.exports = app;
