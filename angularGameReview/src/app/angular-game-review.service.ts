14a76ee3862943e88f279f7356cc14b9256436ee
Jc2772 <jc2772@outlook.com>
2024-12-09 10:50:18 +0000

    angular build does not work

--- angularGameReview/src/app/angular-game-review.service.ts
+++ angularGameReview/src/app/angular-game-review.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { Review } from './home-list/home-list.component';
+import { firstValueFrom } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,15 +10,13 @@ export class AngularGameReviewService {
   private apiBaseurl = 'http://localhost:3000/api';
   public async getReviews(): Promise<Review[]> {
     const url : string = `${this.apiBaseurl}/reviews`;
-    return this.http
-    .get(url)
-    .toPromise()
-    .then(response => {return response as Review[]})
-    .catch(this.handleError)
-  }
-  private handleError(error: any): Promise<any>{
-    console.error('Something has gone Wrong',error)
-    return Promise.reject(error.message || error)
+    try{
+      return await firstValueFrom(this.http.get<Review[]>(url))
+    }
+    catch(error){
+      console.error('Something has gone Wrong',error)
+      return Promise.reject(error)
+    }
   }
   constructor(private http: HttpClient) { }
 }
--- angularGameReview/src/app/app.module.ts
+++ angularGameReview/src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { HomeListComponent } from './home-list/home-list.component';
 import { AngularGameReviewService } from './angular-game-review.service';
+import { provideHttpClient } from '@angular/common/http';
 imports: [
-  BrowserModule,
-  HttpClientModule
+  BrowserModule
 ]
 @NgModule({
   declarations: [
     HomeListComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [HomeListComponent]
 })
 export class AppModule {}
