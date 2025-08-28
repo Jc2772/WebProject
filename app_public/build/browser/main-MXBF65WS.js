1dd6847fc16f79a38054d6cec5fe040c96743274
jc2772 <jc2772@outlook.com>
2025-08-28 17:08:52 +0100

    .

--- app_public/build/browser/main-MXBF65WS.js
+++ app_public/build/browser/main-MXBF65WS.js
@@ -32007,7 +32007,7 @@ var ReviewService = class _ReviewService {
   constructor(http) {
     this.http = http;
   }
-  apiBaseurl = "https://webproject-4kuk.onrender.com/api";
+  apiBaseurl = "https://webproject-3fvz.onrender.com/api";
   FetchReviews() {
     return __async(this, null, function* () {
       const url = `${this.apiBaseurl}/reviews`;
